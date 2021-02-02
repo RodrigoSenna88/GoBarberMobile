f599b342d502c55dd443d331f71d8d46d5525924
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-02-02 16:27:18 -0300

    realização de cadastro com conexão com a api realizado, incluindo alerta de sucesso na realização de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -12,8 +12,8 @@ import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
-
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationsErrors from '../../utils/getValidationsErrors';
 
@@ -56,9 +56,14 @@ const SignUp: React.FC = () => {
           abortEarly: false,
         });
 
-        // await api.post('/users', data);
+        await api.post('/users', data);
 
-        // history.push('/');
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Você já pode fazer login na aplicação.',
+         );
+
+        navigation.goBack();
 
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
