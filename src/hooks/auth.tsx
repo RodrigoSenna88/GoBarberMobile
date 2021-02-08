a788a70dec3f1d546416d09e7ea10ac65663b100
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-02-08 16:08:55 -0300

    atualização dos dados do perfil realizado com sucesso. falta fazer atualização da imagem

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -29,7 +29,7 @@ interface AuthContextData {
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
-  updateUser(user: User): void;
+  updateUser(user: User): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -83,8 +83,8 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   const updateUser = useCallback(
-    (user: User) => {
-      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+    async (user: User) => {
+      await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
       setData({
         token: data.token,
