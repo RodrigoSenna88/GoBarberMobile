4e0707f0b371237c0ac63f74b8811edceea47e76
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-02-08 10:38:15 -0300

    iniciando criação da tela de sucesso. falta customizar o texto de agendamento

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native'
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPress = useCallback(() => {
+    reset({
+      routes: [
+        { name: 'Dashboard' }
+      ],
+      index: 0
+    });
+  }, [reset]);
   return (
-    <View />
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Segunda, dia 08 de Fevereiro de 2021 às 14:00</Description>
+
+      <OkButton onPress={handleOkPress}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
